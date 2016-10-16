c27574e472b3192243fce1f882b74a13d6ac8384
lujun9972 <lujun9972@gmail.com>
2016-10-16 21:27:14 +0800

    add ego config

--- my-life/packages.el
+++ my-life/packages.el
@@ -181,8 +181,29 @@
     :defer  t))
 
 (defun my-life/init-ego ()
-  (with-eval-after-load 'org (use-package ego
-                               :defer  t)))
+  (with-eval-after-load 'org
+    (use-package ego
+      :config
+      (ego-add-to-alist 'ego-project-config-alist
+               `(("emacs-document" ; 站点工程的名字
+                 :repository-directory "~/github/emacs-document" ; 站点的本地目录
+                 :site-domain "http://lujun9972.github.io/emacs-document" ; 站点的网址
+                 :site-main-title "EMACS-DOCUMENT" ; 站点的标题
+                 :site-sub-title "=============>集思广益" ; 站点的副标题
+                 :repository-org-branch "master"
+                 :repository-html-branch "gh-pages"
+                 :theme (default) ; 使用的主题
+                 :summary (("years" :year :updates 10) ("authors" :authors) ("tags" :tags)) ; 导航栏的设置，有 category 和 summary 两种
+                 :source-browse-url ("Github" "https://github.com/lujun9972/emacs-document") ; 你的工程源代码所在的位置
+                 :personal-disqus-shortname "emacs-document" ; 使用 disqus 评论功能的话，它的短名称
+                 :personal-duoshuo-shortname "emacs-document" ; 使用 多说 评论功能的话，它的短名称
+                 ;; :confound-email nil ; 是否保护邮件名称呢？t 是保护，nil 是不保护，默认是保护
+                 :ignore-file-name-regexp "README.org" ; 有些不想发布成 html 的 org 文件（但是又想被导入 git 进行管理），可以用这种正则表达的方式排除
+                 :web-server-docroot "~/webRoot/emacs-document.github.io" ; 本地测试的目录
+                 :web-server-port 5432); 本地测试的端口
+
+                 ;; 你可以在此添加更多的站点设置
+               )))))
 
 (defun my-life/init-org2issue ()
   (with-eval-after-load 'org (use-package org2issue
