f9915bdf4c3f91777d7b7fd90332754a002b0adf
darksun <lujun9972@gmail.com>
2017-03-07 20:36:19 +0800

    remove pocket-mode

--- init.el
+++ init.el
@@ -91,7 +91,7 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(request json-mode pocket-api pocket-mode)
+   dotspacemacs-additional-packages '(request json-mode pocket-api)
    ;; A list of packages and/or extensions that will not be install and loaded.
    dotspacemacs-excluded-packages '()
    ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that
