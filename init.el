ab2da2a8e3c9ba4238999409192e58014ae584c9
darksun <lujun9972@gmail.com>
2017-06-27 18:03:24 +0800

    add reading layers for reading

--- init.el
+++ init.el
@@ -104,12 +104,13 @@ values."
       my-eshell
       my-program
       my-life
+      reading
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(request json-mode pocket-api interleave)
+   dotspacemacs-additional-packages '(request json-mode pocket-api)
    ;; A list of packages and/or extensions that will not be install and loaded.
    dotspacemacs-excluded-packages '()
    ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that
