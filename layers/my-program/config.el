eb6746de1d937a10719edd1cca29115472648e53
darksun <lujun9972@gmail.com>
2018-02-21 11:49:53 +0800

    remove imenubar

--- layers/my-program/config.el
+++ layers/my-program/config.el
@@ -1,5 +1,5 @@
 ;;开启imenu菜单,可以通过Index菜单来跳转到各个defun的地方
-(add-hook 'prog-mode-hook 'imenu-add-menubar-index)
+;; (add-hook 'prog-mode-hook 'imenu-add-menubar-index)
 (set-default 'imenu-auto-rescan t)
 
 ;; 配置vc-git在win环境下，用gbk编码格式进行提交
