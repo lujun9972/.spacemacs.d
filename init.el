47e88a5ea0a64ee87688f80d661945a15bffc946
darksun <lujun9972@gmail.com>
2018-02-27 14:30:34 +0800

    update

--- init.el
+++ init.el
@@ -120,6 +120,7 @@ values."
       my-eshell
       my-program
       my-life
+      my-present
       reading
       pdf-tools
      )
@@ -127,7 +128,7 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(request json-mode all-the-icons-dired edit-indirect)
+   dotspacemacs-additional-packages '(request json-mode all-the-icons-dired edit-indirect anki-editor)
    ;; A list of packages and/or extensions that will not be install and loaded.
    dotspacemacs-excluded-packages '()
    ;; If non-nil spacemacs will delete any orphan packages, i.e. packages that
