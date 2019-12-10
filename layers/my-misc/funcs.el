8c185edc8086bf13fb4f5afabb48b2a7b53f4805
darksun <lujun9972@gmail.com>
2019-12-10 22:45:28 +0800

    update

--- layers/my-misc/funcs.el
+++ layers/my-misc/funcs.el
@@ -9,3 +9,41 @@
   (interactive (find-function-read))
   (find-function-do-it fun nil 'switch-to-buffer)
   (view-mode 1))
+
+
+;; 用org-mode保存firefox session
+(defun save-firefox-session ()
+  "Reads chromium current session and converts it to org-mode chunk."
+  (interactive)
+  (save-excursion
+    (let* ((path "~/.mozilla/firefox/*.default/sessionstore-backups/recovery.jsonlz4")
+           (cmd (concat "lz4jsoncat " path " | grep -oP '\"(http.+?)\"' | sed 's/\"//g' | sort | uniq"))
+           (ret (shell-command-to-string cmd)))
+      (insert
+       (concat
+        "* "
+        (format-time-string "[%Y-%m-%d %H:%M:%S]")
+        "\n"
+        (mapconcat 'identity
+                   (cl-reduce (lambda (lst x)
+                                (if (and x (not (string= "" x)))
+                                    (cons (concat " - " x) lst)
+                                  lst))
+                              (split-string ret "\n")
+                              :initial-value (list))
+                   "\n"))))))
+
+(defun restore-firefox-session ()
+  "Restore session, by openning each link in list with (browse-url).
+  Make sure to put cursor on date heading that contains list of urls."
+  (interactive)
+  (save-excursion
+    (beginning-of-line)
+    (when (looking-at "^\\*")
+      (forward-line 1)
+      (while (looking-at "^[ ]+-[ ]+\\(http.?+\\)$")
+        (let* ((ln (thing-at-point 'line t))
+               (ln (replace-regexp-in-string "^[ ]+-[ ]+" "" ln))
+               (ln (replace-regexp-in-string "\n" "" ln)))
+          (browse-url ln))
+        (forward-line 1)))))
