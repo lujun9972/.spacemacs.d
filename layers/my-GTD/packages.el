766d4c3974909740640f7bb24ecd35937fb78b62
darksun <lujun9972@gmail.com>
2020-02-18 20:45:07 +0800

    update

--- layers/my-GTD/packages.el
+++ layers/my-GTD/packages.el
@@ -32,13 +32,14 @@
     (setq appt-audible t)               ;开启appointment的声音提醒
     (setq appt-display-mode-line t)     ;在mode-line上显示appointment的倒计时
     (setq appt-message-warning-time 30) ;提前半个小时提醒
+    (setq appt-display-interval 5)      ;每5分钟提醒一次
     (add-hook 'diary-hook 'appt-make-list)
     ;; 设置使用桌面通知提醒
     (require 'notifications)
     (defun appt-disp-window-and-notification (min-to-appt current-time appt-msg)
       "自定义的appt通知函数，会使用notifications发送桌面通知"
       (let ((title (format "%s分钟内有新的任务" min-to-appt)))
-        (notifications-notify :timeout 0    ;不自动消失
+        (notifications-notify :timeout (* appt-display-interval 60000) ;超时时间为下一次提醒的时间
                               :title title
                               :body appt-msg)
         (appt-disp-window min-to-appt current-time appt-msg))) ;同时也调用原有的提醒函数
